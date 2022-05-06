8ec0d5eca15c36d3cea375428622339bf1ecb1e1
Michael Pacheco <48970112+MichaelPachec0@users.noreply.github.com>
2022-05-05 21:25:57 -0700

    Cleanup ai JS.

--- js/ai.mjs
+++ js/ai.mjs
@@ -8,9 +8,8 @@ export class Ai {
         this.id = 2;
         //reference to parent object
         this.p = p;
-        // Ai will start from 0 easiest to 2 hardest, TODO: Allow this to be set by the parent class
+        // Ai will start from 0 easiest to 2 hardest
         this.type = type;
-        //map with Ai move to list of lists of winning strategies
         this.moves = new Map();
         this.possible = [...Array(rowSize ** 2).keys()]
         this.playerMoves = [];
@@ -18,32 +17,27 @@ export class Ai {
         this.tree = new Map();
     }
 
-    // ((i < (arr.length/rows)) ? 1 : 0) * 2 + i *
     choice(playerMove) {
         if (this.p.won){
-            return
+            return;
         }
-        let move = +playerMove
-        this.playerMoves.push(move)
-        // corners
-        // if (this.moves.size === 0) {
-        this.updateMove(move)
-        // console.log(this.playerMoves, playerMove, this.possible)
+        const move = Number(playerMove);
+        this.playerMoves.push(move);
+        this.updateMove(move);
         switch(this.type){
             case 0:
-                return this.easyAI()
+                return this.easyAI();
             case 1:
-                return this.randomAI()
+                return this.randomAI();
             default:
-                return this.impossibleAI()
-                // return this.impossibleAI()
+                return this.impossibleAI();
 
         }
     }
 
     updateMove(move){
-        this.possible = this.possible.filter(e=> e!==move)
-        this.moves.set(move, 1)
+        this.possible = this.possible.filter(e=> e!==move);
+        this.moves.set(move, 1);
 
     }
 
@@ -53,7 +47,7 @@ export class Ai {
      */
     easyAI() {
         let move = this.possible[Math.floor(Math.random()*this.possible.length)];
-        this.updateMove(move)
+        this.updateMove(move);
         return move;
     }
 
@@ -71,54 +65,54 @@ export class Ai {
     }
 
     impossibleAI(){
-        let bestScore = Infinity
-        let move
+        // TODO: Alpha-beta pruning and depth limiter.
+        let bestScore = Infinity;
+        let move;
         for (const candidate  of this.possible){
-            this.p.setBoardSquare = {square: candidate, player: this.id}
-            let score = this.mm(this.possible.filter((e) => e !== candidate), 0, 1, [candidate])
-            this.p.resetSquare = candidate
-            this.tree.set(candidate, score)
+            this.p.setBoardSquare = {square: candidate, player: this.id};
+            const score = this.mm(this.possible.filter((e) => e !== candidate), 0, 1, [candidate]);
+            this.p.resetSquare = candidate;
+            this.tree.set(candidate, score);
             if (score < bestScore){
-                bestScore = score
-                move = candidate
+                bestScore = score;
+                move = candidate;
             }
         }
-        this.tree = new Map()
-        this.updateMove(move)
-        return move
+        this.tree = new Map();
+        this.updateMove(move);
+        return move;
     }
     /**
      * minimax implemented using info from https://en.wikipedia.org/wiki/Minimax adapted for
      * use here.
      * @param possible {Array<number>} possible moves to make
-     * @param depth recursive check for knowing how far we should go
+     * @param depth {number} recursive check for knowing how far we should go
      * @param player {number} 1 or 2 depending on the player
      * @param moves {Array<number>} tracking moves in the simulation.
      * @return {number} will return a score, a number of how good the move is in accordance with
      *  the calculated optimal move for the adversary to move on.
      */
     mm(possible, depth, player, moves) {
-        let res = this.p.checkWinnerHelper((player === 1) ? 2 : 1, moves.length, true)
+        const res = this.p.checkWinnerHelper((player === 1) ? 2 : 1, moves.length, true);
         if (res !== null) {
-            return this.score.get(res) - depth
+            return this.score.get(res) - depth;
         }
-        let bScore = (player === 1)? -Infinity : Infinity
-        // for (let i = rowSize**2;i--;){
+        let bScore = (player === 1)? -Infinity : Infinity;
         for (const candidate of possible){
             if(this.p.squareIsNotOwned(candidate)){
-                this.p.setBoardSquare = {square: candidate, player: player}
-                moves.push(candidate)
-                let score = this.mm(possible.filter((e) => e !== candidate), depth+1, (player === 1)? 2:1, moves)
-                this.p.resetSquare = moves.pop()
-                bScore = (player === 1)? Math.max(score, bScore) : Math.min(score, bScore)
+                this.p.setBoardSquare = {square: candidate, player: player};
+                moves.push(candidate);
+                let score = this.mm(possible.filter((e) => e !== candidate), depth+1, (player === 1)? 2:1, moves);
+                this.p.resetSquare = moves.pop();
+                bScore = (player === 1)? Math.max(score, bScore) : Math.min(score, bScore);
             }
         }
-        return bScore
+        return bScore;
     }
     setAi(type){
-        this.type = +type
-        this.reset()
-        console.log(`SET AI TO ${type}`)
+        this.type = +type;
+        this.reset();
+        console.log(`SET AI TO ${type}`);
     }
     reset(){
         this.moves = new Map();
